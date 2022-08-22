62cf2be5f99c812c0e8188fc64c6fe0fb167a581
KaTeE234 <mashenykamon007@gmail.com>
2022-08-22 11:48:30 +0300

    Integrated API

--- weather-app/src/Weather.js
+++ weather-app/src/Weather.js
@@ -1,74 +1,82 @@
 import React, { useState } from "react";
 import "./Weather.css";
+import axios from "axios";
 
 export default function Weather() {
-  let weatherData = {
-    date: "Tuesday 00:00",
-    description: "Cloudy",
-    imgUrl: "https://cdn-icons-png.flaticon.com/512/2983/2983922.png",
-    feels: "12",
-    humidity: "80",
-    pressure: "1017",
-    wind: "1",
-  };
-  const [city, setCity] = useState("");
-  const [result, setResult] = useState("Seoul");
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    setResult(city);
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  function handleResponse(response) {
+    console.log(response.data);
+    setWeatherData({
+      ready: true,
+      date: "Tuesday 00:00",
+      temperature: response.data.main.temp,
+      feels: Math.round(response.data.main.feels_like),
+      humidity: response.data.main.humidity,
+      pressure: response.data.main.pressure,
+      city: response.data.name,
+      wind: response.data.wind.speed,
+      description: response.data.weather[0].description,
+      icon: "https://cdn-icons-png.flaticon.com/512/2983/2983922.png",
+    });
   }
 
-  function changeCity(event) {
-    event.preventDefault();
-    setCity(event.target.value);
-  }
-
-  return (
-    <div className="Weather">
-      <div className="main-background">
-        <h1>Weather Forecast</h1>
-        <form className="search-area" onSubmit={handleSubmit}>
-          <input
-            type="text"
-            placeholder="Search city"
-            id="text-city"
-            autoComplete="off"
-            onChange={changeCity}
-          />
-          <input type="submit" value="🔎" className="search-button" />
-          <button className="location">Current location</button>
-        </form>
-
-        <h4 className="today">Last updated: {weatherData.date}</h4>
-        <div className="row forecast">
-          <div className="col-4">
-            <h3>{result}</h3>
-            <p className="description">{weatherData.description}</p>
-          </div>
-          <div className="col-4 main-temperature">
-            <span className="temperature">14</span>
-            <span className="unit"> °C </span>
-            <img
-              src={weatherData.imgUrl}
-              className="main-image"
-              alt={weatherData.description}
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <div className="main-background">
+          <h1>Weather Forecast</h1>
+          <form className="search-area">
+            <input
+              type="text"
+              placeholder="Search city"
+              id="text-city"
+              autoComplete="off"
             />
+            <input type="submit" value="🔎" className="search-button" />
+            <button className="location">Current location</button>
+          </form>
+
+          <h4 className="today">Last updated: {weatherData.date}</h4>
+          <div className="row forecast">
+            <div className="col-4">
+              <h3>London</h3>
+              <p className="description">{weatherData.description}</p>
+            </div>
+            <div className="col-4 main-temperature">
+              <span className="temperature">
+                {Math.round(weatherData.temperature)}
+              </span>
+              <span className="unit"> °C </span>
+              <img
+                src={weatherData.icon}
+                className="main-image"
+                alt={weatherData.description}
+              />
+            </div>
+            <div className="col-4">
+              <span className="info">
+                <span>Feels like: {weatherData.feels}</span>
+                <br />
+                <span>Humidity: {weatherData.humidity}%</span>
+                <br />
+                <span>Pressure: {weatherData.pressure}</span>
+                <br />
+                <span>Wind speed: {Math.round(weatherData.wind)} km/h</span>
+              </span>
+            </div>
           </div>
-          <div className="col-4">
-            <span className="info">
-              <span>Feels like: {weatherData.feels}</span>
-              <br />
-              <span>Humidity: {weatherData.humidity}%</span>
-              <br />
-              <span>Pressure: {weatherData.pressure}</span>
-              <br />
-              <span>Wind speed: {weatherData.wind} km/h</span>
-            </span>
-          </div>
+          <h4 className="week">5 Day Forecast</h4>
         </div>
-        <h4 className="week">5 Day Forecast</h4>
       </div>
-    </div>
-  );
+    );
+  } else {
+    let city = "London";
+    let units = "metric";
+    let apiKey = "0727199221e468420f29d7fe1ea9bf71";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading...";
+  }
 }
